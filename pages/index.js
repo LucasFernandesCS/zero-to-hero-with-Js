d075f2478476b86ecf2e91b00b2216c0f471a011
Lucas Fernandes <Lucasfernandescs@outlook.com>
2025-11-24 18:59:36 -0300

    Adicionando página em construção

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,47 @@
 function Home() {
-  return <h1>Entenda que eu te amo, denguinho</h1>;
+  return (
+    <>
+      <style>
+        {`
+          body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 0;
+            font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+          }
+
+          h1 {
+            font-size: 3rem;
+            color: #333;
+            text-align: center;
+          }
+
+          p {
+            font-size: 1.5rem;
+            color: #333;
+            text-align: center;
+          }
+
+          @media (max-width: 600px) {
+            h1 {
+              font-size: 2rem;
+            }
+
+            p {
+              font-size: 1rem;
+            }
+          }
+        `}
+      </style>
+      <h1>Em Construção</h1>
+      <p>
+        Esta ferramenta está em desenvolvimento para ajudá-lo a organizar suas
+        atividades, tarefas e serviços.
+      </p>
+    </>
+  );
 }
 
 export default Home;
